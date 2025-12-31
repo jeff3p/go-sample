787aa5ce6143a32928e54cdb9cd56eaaa37df1c9
jeff3p <jeff3p@gmail.com>
2025-12-31 13:43:11 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
+
 package main
 
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
+        // Read env vars inline
+        sample := os.Getenv("SAMPLE")
+        if sample == "" {
+            sample = "(unset)"
+        }
+        sample2 := os.Getenv("SAMPLE2")
+        if sample2 == "" {
+            sample2 = "(unset)"
+        }
+
         switch r.URL.Path {
         case "/":
-            fmt.Fprint(w, "document root")
+            fmt.Fprintf(w, "document root\nSAMPLE=%s\nSAMPLE2=%s\n", sample, sample2)
         case "/banana":
-            fmt.Fprint(w, "banana")
+            fmt.Fprintf(w, "banana\nSAMPLE=%s\nSAMPLE2=%s\n", sample, sample2)
         default:
-            // Keep it simple: no logging, default 404
             http.NotFound(w, r)
         }
     })
