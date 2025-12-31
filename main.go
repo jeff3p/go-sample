ec242a8ca35e28744e7d2eaa90d4fec372c4a9b7
jeff3p <jeff3p@gmail.com>
2025-12-31 11:54:55 -0500

    Update main.go

--- main.go
+++ main.go
@@ -56,11 +56,11 @@ func main() {
 	log.Printf("Starting server on port %s\n", port)
 	log.Println("Access it at http://localhost" + port)
 
-	sample := os.Getenv("SAMPLE")
- 	log.Println("Environment variable loaded" + sample)
+	//sample := os.Getenv("SAMPLE")
+ 	//log.Println("Environment variable loaded" + sample)
 
-	sample2 := os.Getenv("SAMPLE2")
-	log.Println("Environment variable loaded" + sample2)
+	//sample2 := os.Getenv("SAMPLE2")
+	//log.Println("Environment variable loaded" + sample2)
 
 
 	log.SetFlags(log.LstdFlags)
